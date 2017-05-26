d38dc841c157620c08ec2d6c97921258842cc278
luojingyu <1246000821@qq.com>
2017-05-26 14:15:59 +0800

    Revert "解决第二题"
    This reverts commit 51867cd2314efdf1c36b5586dc4eb99c488eb2d9.

--- src/Problem1.java
+++ src/Problem1.java
@@ -1,29 +1,7 @@
+package PACKAGE_NAME;
+
 /**
  * Created by luojingyu on 17-5-24.
  */
-import java.util.HashSet;
-import java.util.Random;
-
 public class Problem1 {
-    public static void main(String[] args) {
-        Random random = new Random();
-        Object[] values = new Object[200];
-
-        HashSet<Integer> hashSet = new HashSet<Integer>();
-
-        while (hashSet.size() < values.length) {
-            hashSet.add(random.nextInt(600));
-        }
-
-        values = hashSet.toArray();
-
-        for(int i=0; i<values.length; i++) {
-            System.out.print(values[i]);
-            System.out.print(" ");
-
-            if((i+1) % 10 == 0) {
-                System.out.println("\n");
-            }
-        }
-    }
 }
