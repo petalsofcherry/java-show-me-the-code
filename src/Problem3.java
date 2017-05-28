cdd5c754ff0253416315402cb29455af0cddbd7f
luojingyu <1246000821@qq.com>
2017-05-28 11:33:38 +0800

    提交第三题

--- src/Problem3.java
+++ src/Problem3.java
@@ -1,7 +1,34 @@
-package PACKAGE_NAME;
+import java.util.HashSet;
+import java.util.Random;
+
+import redis.clients.jedis.Jedis;
 
 /**
  * Created by luojingyu on 17-5-28.
  */
 public class Problem3 {
+
+    public static Object[] getRandom() {
+        Random random = new Random();
+        Object[] values = new Object[200];
+
+        HashSet<Integer> hashSet = new HashSet<Integer>();
+
+        while (hashSet.size() < values.length) {
+            hashSet.add(random.nextInt(600));
+        }
+
+        values = hashSet.toArray();
+        return values;
+    }
+
+    public static void main (String[] args) {
+        Jedis jedis = new Jedis("localhost");
+        Object[] myRandomNum = getRandom();
+        for (int i=0; i<myRandomNum.length; i++) {
+            jedis.lpush("mylist", String.valueOf(myRandomNum[i]));
+        }
+        jedis.close();
+    }
+
 }
