1254b0c7d32b43fd645a8ffb737c9d5e1630ef15
luojingyu <1246000821@qq.com>
2017-06-02 10:43:50 +0800

    完成问题8,9

--- src/Probelm8_9.java
+++ src/Probelm8_9.java
@@ -1,7 +1,26 @@
-package PACKAGE_NAME;
-
 /**
  * Created by luojingyu on 17-6-2.
  */
+import org.jsoup.*;
+import org.jsoup.nodes.*;
+import org.jsoup.select.*;
+
+
 public class Probelm8_9 {
+    public static void main(String[] args) {
+        try {
+            Document doc = Jsoup.connect("http://www.jianshu.com/p/bf21fb55bd8c").get();
+            Elements body = doc.getElementsByTag("body");
+            for (Element i: body) {
+                System.out.println(i.text());
+            }
+            Elements links = doc.getElementsByTag("a");
+            for (Element link: links) {
+                System.out.println(link.attr("href"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
